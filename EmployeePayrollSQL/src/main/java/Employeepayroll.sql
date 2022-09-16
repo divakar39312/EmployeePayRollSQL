0ba0c34b435d51143fda58945583fc26931482d4
Divakar <divakar39312@gmail.com>
2022-09-16 14:48:39 +0530

    [Divakar]:create database

---
+++ EmployeePayrollSQL/src/main/java/Employeepayroll.sql
@@ -0,0 +1,33 @@
+---------------- UC1 ----------------
+
+mysql> create database payroll_services;
+Query OK, 1 row affected (2.75 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| addressbook_db     |
+| firstdatabase      |
+| flight             |
+| flights            |
+| information_schema |
+| location_web_app   |
+| login_app_db       |
+| mysql              |
+| payroll_services   |
+| performance_schema |
+| projectdb1         |
+| seconddatabase     |
+| student_app_db     |
+| sys                |
+| user_app_demo      |
+| userid             |
+| userregister       |
+| web_app2           |
++--------------------+
+18 rows in set (0.24 sec)
+
+mysql> use payroll_services;
+Database changed
+
